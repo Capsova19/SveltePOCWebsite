248819dc3f4ae19e53c3b1a41f059e3c612a0312
Capsova <sovamax19@gmail.com>
2025-08-18 00:43:42 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Button from "$lib/components/ui/button/button.svelte";
+	import { toast } from "svelte-sonner";
+
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button onclick={() => toast.success("Hello world")}>Show toast</Button>
